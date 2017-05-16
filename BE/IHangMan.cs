0c4a363e53e49b30e9aa0f2d93e24d19ab645dcf
Edgaras <test@test.com>
2017-05-16 11:32:38 +0200

    test

--- BE/IHangMan.cs
+++ BE/IHangMan.cs
@@ -26,7 +26,7 @@ namespace BE
 
         public void AddKgs(int kgs)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(); 
         }
     }
 }
