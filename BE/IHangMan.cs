ada1d8ac35aae49dc09816a670f5bcc7dafcc502
Miroslav Pakanec <miropakanec@gmail.com>
2017-05-16 11:16:48 +0200

    BE

---
+++ BE/IHangMan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+
+namespace BE
+{
+    public interface IHangMan
+    {
+        
+    }
+
+    [ContractClassFor(typeof(IHangMan))]
+    internal abstract class TestContract : IHangMan
+    {
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(GetWeight() >= 0);
+        }
+
+        public int GetWeight()
+        {
+            Contract.Ensures(Contract.Result<string>()!= null);
+            throw new NotImplementedException();
+        }
+
+        public void AddKgs(int kgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BE/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Test : ITest
+    {
+        public int GetWeight()
+        {
+            Contract.Ensures(Contract.Result<string>()!= null);
+            throw new NotImplementedException();
+        }
+
+        public void AddKgs(int kgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
